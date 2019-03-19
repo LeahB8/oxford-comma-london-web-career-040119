a77fb1fbbc7e19509af33a73d473742a6bfb220b
Leah Boyd <leahboyd08@gmail.com>
2019-03-19 13:35:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,12 +9,3 @@ def oxford_comma(array)
     nil
   end
 end
-
-
- elsif array.length == 2 
-    array.join(' and ')
-    elsif array.length >= 3 
-      array[-1] = "and #{array[-1]}"
-      array.join(', ')
-    else 
-      nil 
