6efc5fd367e01417be209c4a24d80b8cdfe658bf
Leah Boyd <leahboyd08@gmail.com>
2019-03-19 13:26:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
   if array.size == 2
-    array.join("and")
+    array.join(" and ")
   if array.size == 3
-    array.join(",", ",", "and")
+    array.join(", ", ", ", " and ")
   if array.size >= 4
-    array.join(",", ",", ",", "and")
+    array.join(", ", ", ", ", ", " and ")
   end
 end
