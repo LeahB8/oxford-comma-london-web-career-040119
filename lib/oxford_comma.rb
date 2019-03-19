7fe32b478ca2ce738527b34beeff624cc735b7d0
Leah Boyd <leahboyd08@gmail.com>
2019-03-19 13:31:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,8 +4,8 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size == 3
-    array.join(0..1)
+    array.join(", ", ", ", " and ")
   elsif array.size >= 4
-    array.join(0..1)
+    array.join(", ", ", ", ", ", " and ")
   end
 end
