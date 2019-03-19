6ab098b93d49cbb327df0299d0957b346268256d
Leah Boyd <leahboyd08@gmail.com>
2019-03-19 13:18:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.size == 2
+    array.join("and")
+  elsif array.size == 3
+    array.join(",", ",", "and")
+  elsif array.size == 4
+    array.join(",", ",", ",", "and")
 end
